60db32b2ebd5cd7317cc8b2af0aa9b0bcdeb7b00
suzy <suzyp0223@gmail.com>
2025-04-25 17:11:00 +0900

    페이지네이션

--- github/src/ListContainer.js
+++ github/src/ListContainer.js
@@ -6,11 +6,16 @@ import Button from "./components/Button";
 import ListItem from "./components/ListItem";
 import ListItemLayout from "./components/ListItemLayout";
 import Modal from "./components/Modal";
+import Pagination from "./components/Pagination";
 
 const ListContainer = () => {
   // 리액트에서 input을 다룰땐 useState로 많이 다룸.
   const [inputValue, setInputValue] = useState("is:pr is:open");
   const [list, setList] = useState([]);
+
+  // currentPage 변경 state
+  const [page, setPage] = useState(1);
+  // const MAX_PAGE = getData().totalCount
 
   // 리스트 체크박스
   // const [checkedList, setCheckedList] = useState([]);
@@ -30,86 +35,99 @@ const ListContainer = () => {
   // }, [inputValue]);
 
   return (
-    <div className={styles.listContainer}>
-      <div className={styles.topSection}>
-        <input
-          className={styles.input}
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        <Button
-          style={{ fontSize: "14px", backgroundColor: "green", color: "white" }}
-        >
-          New Issue
-        </Button>
+    <>
+      <div className={styles.listContainer}>
+        <div className={styles.topSection}>
+          <input
+            className={styles.input}
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <Button
+            style={{
+              fontSize: "14px",
+              backgroundColor: "green",
+              color: "white",
+            }}
+          >
+            New Issue
+          </Button>
+        </div>
+        <>
+          {/* <span
+            role="button"
+            className={cx(styles.textFilter, { [styles.selected]: isOpenMode })}
+            onClick={() => setIsOpenMode(true)}
+          >
+            {openModeDataSize}Open
+          </span>
+          <span
+            role="button"
+            className={cx(styles.textFilter, {
+              [styles.selected]: !isOpenMode,
+            })}
+            onClick={() => setIsOpenMode(false)}
+          >
+            {closeModeDataSize}Closed
+          </span> */}
+
+          {/* 반복되는 코드를  컴포넌트로 분리 2*/}
+          {/* <OpenClosedFilter
+            size={openModeDataSize}
+            state="Open"
+            selected={isOpenMode}
+            onClick={() => setIsOpenMode(true)}
+          />
+          <OpenClosedFilter
+            size={closeModeDataSize}
+            state="Closed"
+            selected={!isOpenMode}
+            onClick={() => setIsOpenMode(false)}
+          /> */}
+        </>
+
+        <OpenClosedFilters />
+        <ListItemLayout className={styles.listFilter}>
+          <ListFilter
+            onChangeFilter={(filteredData) => {
+              // 필터링된 요소에 맞게 데이터를 불러오기
+              // const data = getDate("필터링된 정보");
+              // setList(data);
+            }}
+          />
+        </ListItemLayout>
+        <div className={styles.container}>
+          <ListItem
+            // checked={checkedList.filter((item) => item.id === "0")[0]}
+            // onChangeCheckBox={() => {
+            //   const currentChecked = checkedList.filter(
+            //     (item) => item.id === "0",
+            //   )[0];
+
+            //   if (currentChecked) {
+            //     // 리스트에서 빼기
+            //   } else {
+            //     // 리스트에 추가하기
+            //   }
+            //   setCheckedList((checkedList) => [...checkedList, "0"]);
+            // }}
+            badges={[
+              {
+                color: "red",
+                title: "Bug2",
+              },
+            ]}
+          />
+          <div className={styles.paginationContainer}>
+            <Pagination
+              maxPage={10}
+              currentPage={page}
+              onClickPageButton={(number) => setPage(number)}
+            />
+          </div>
+        </div>
       </div>
-      <>
-        {/* <span
-          role="button"
-          className={cx(styles.textFilter, { [styles.selected]: isOpenMode })}
-          onClick={() => setIsOpenMode(true)}
-        >
-          {openModeDataSize}Open
-        </span>
-        <span
-          role="button"
-          className={cx(styles.textFilter, {
-            [styles.selected]: !isOpenMode,
-          })}
-          onClick={() => setIsOpenMode(false)}
-        >
-          {closeModeDataSize}Closed
-        </span> */}
-
-        {/* 반복되는 코드를  컴포넌트로 분리 2*/}
-        {/* <OpenClosedFilter
-          size={openModeDataSize}
-          state="Open"
-          selected={isOpenMode}
-          onClick={() => setIsOpenMode(true)}
-        />
-        <OpenClosedFilter
-          size={closeModeDataSize}
-          state="Closed"
-          selected={!isOpenMode}
-          onClick={() => setIsOpenMode(false)}
-        /> */}
-      </>
-
-      <OpenClosedFilters />
-      <ListItemLayout className={styles.listFilter}>
-        <ListFilter
-          onChangeFilter={(filteredData) => {
-            // 필터링된 요소에 맞게 데이터를 불러오기
-            // const data = getDate("필터링된 정보");
-            // setList(data);
-          }}
-        />
-      </ListItemLayout>
-      <div className={styles.container}>
-        <ListItem
-          // checked={checkedList.filter((item) => item.id === "0")[0]}
-          // onChangeCheckBox={() => {
-          //   const currentChecked = checkedList.filter(
-          //     (item) => item.id === "0",
-          //   )[0];
-
-          //   if (currentChecked) {
-          //     // 리스트에서 빼기
-          //   } else {
-          //     // 리스트에 추가하기
-          //   }
-          //   setCheckedList((checkedList) => [...checkedList, "0"]);
-          // }}
-          badges={[
-            {
-              color: "red",
-              title: "Bug2",
-            },
-          ]}
-        />
-      </div>
-    </div>
+    </>
   );
 };
 
---
+++ github/src/components/Pagination.js
@@ -0,0 +1,51 @@
+import cx from "clsx";
+
+import styles from "./Pagination.module.css";
+
+// 현재어떤 페이지를 보고있는지-currentPage, 최대페이지-maxPage
+const Pagination = ({ currentPage, maxPage, onClickPageButton }) => {
+  return (
+    <div>
+      <button
+        // className={cx(styles.button, { [styles.disabled]: currentPage === 1 })}
+        className={cx(styles.button, styles.blueButton)}
+        disabled={currentPage === 1}
+      >
+        {"< Previous"}
+      </button>
+      {new Array(maxPage).fill(null).map((_, idx) => {
+        const number = idx + 1;
+        return (
+          <PageButton
+            key={`page-${number}`}
+            number={number}
+            onClick={onClickPageButton}
+            selected={number === currentPage}
+          />
+        );
+      })}
+      <button
+        // className={cx(styles.button, {[styles.disabled]: currentPage === maxPage})}
+        className={cx(styles.button, styles.blueButton)}
+        disabled={currentPage === maxPage}
+      >
+        {"Next >"}
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
+
+// 페이지네이션 안 버튼
+//onClick을 밖에서 받아야 커스터마이징이 가능.
+function PageButton({ onClick, number, selected }) {
+  return (
+    <button
+      className={cx(styles.button, { [styles.selected]: selected })}
+      onClick={() => onClick(number)}
+    >
+      {number}
+    </button>
+  );
+}
