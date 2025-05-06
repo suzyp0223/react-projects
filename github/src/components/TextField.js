d509b7a6fd4096b369b54fae7c3156b8b1f87ba4
suzy <suzyp0223@gmail.com>
2025-05-06 19:26:38 +0900

    ref={ref}

--- github/src/components/TextField.js
+++ github/src/components/TextField.js
@@ -7,12 +7,14 @@ const TextField = ({ type = "input", name, placeholder }, ref) => {
   return type === "input" ? (
     <input
       name={name}
+      ref={ref}
       className={cx(styles.input, styles.border)}
       placeholder={placeholder}
     />
   ) : (
     <textarea
       name={name}
+      ref={ref}
       className={cx(styles.textarea, styles.input)}
       placeholder={placeholder}
     ></textarea>
