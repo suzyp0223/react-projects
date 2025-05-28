93a44a1646281949a5e3f61083afd463564771fb
suzy <suzyp0223@gmail.com>
2025-05-28 16:11:28 +0900

    � Stop tracking .env and update configs

--- 30-project/4-8github-search-engine/vite.config.js
+++ 30-project/4-8github-search-engine/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // <- 여기 디렉터리 이름을 꼭 확인하세요
+  },
+});
