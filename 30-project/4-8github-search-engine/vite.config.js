2a3e6c6b420b6d426609c0fde17df0dbf7523dce
suzy <suzyp0223@gmail.com>
2025-05-28 16:13:29 +0900

    파이어베이스 firebase

--- 30-project/4-8github-search-engine/vite.config.js
+++ 30-project/4-8github-search-engine/vite.config.js
@@ -7,4 +7,9 @@ export default defineConfig({
   build: {
     outDir: "dist", // <- 여기 디렉터리 이름을 꼭 확인하세요
   },
+  define: {
+    "import.meta.env.VITE_GITHUB_TOKEN": JSON.stringify(
+      import.meta.env.VITE_GITHUB_TOKEN
+    ),
+  },
 });
